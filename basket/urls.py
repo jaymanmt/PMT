74b3418518033115d46bd2662c90b03ba39883b6
Ubuntu <ubuntu@ip-172-31-93-179.ec2.internal>
2019-11-14 08:18:01 +0000

    able to add quantity to basket and not route back to the shop

--- basket/urls.py
+++ basket/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import showbasket, addtobasket, removefrombasket
+from .views import showbasket, addtobasket, removefrombasket, addtobasketfrombasket
 
 urlpatterns = [
     path('', showbasket, name='showbasket'),
     path('add-item-to-basket/<product_id>', addtobasket, name="addtobasket"),
-    path('remove-item-from-basket/<bkt_item_id>', removefrombasket, name="removefrombasket")
+    path('remove-item-from-basket/<bkt_item_id>', removefrombasket, name="removefrombasket"),
+    path('add-item-to-basket-from-basket/<product_id>', addtobasketfrombasket, name="addtobasketfrombasket"),
 ]
--- basket/views.py
+++ basket/views.py
@@ -43,6 +43,26 @@ def addtobasket(request, product_id):
         existing_bkt_item.save()
     return redirect(reverse('shop')+'?added=1')
 
+def addtobasketfrombasket(request, product_id):
+    # determine which product we are buying
+    product = Item.objects.get(pk=product_id)
+    
+    # if the product already exists in the user's shopping basket
+    existing_bkt_item = basketItem.objects.filter(owner=request.user, product=product).first()
+    
+    # if the basket item does not exist, create new one
+    if existing_bkt_item == None:
+        new_bkt_item = basketItem()
+        new_bkt_item.product = product
+        new_bkt_item.owner = request.user
+        new_bkt_item.quantity_to_buy = 1
+        new_bkt_item.save()
+    else:
+        # increases its quantity
+        existing_bkt_item.quantity_to_buy += 1
+        existing_bkt_item.save()
+    return redirect(reverse('showbasket')+'?added=1')
+    
 def removefrombasket(request, bkt_item_id):
     existing_bkt_item = basketItem.objects.filter(pk=bkt_item_id)
     for each_bkt_item in existing_bkt_item:
