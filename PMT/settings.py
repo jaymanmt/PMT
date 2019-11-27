ffb5ff5f55d12416540ab0d91a8faed6150e88c5
Ubuntu <ubuntu@ip-172-31-93-179.ec2.internal>
2019-11-27 09:00:19 +0000

    deploye update5

--- PMT/settings.py
+++ PMT/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["sgmuaythai.herokuapp.com"]
+ALLOWED_HOSTS = ["sgmuaythai.herokuapp.com","www.sgmuaythai.org"]
 
 
 # Application definition
